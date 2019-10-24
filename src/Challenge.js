2a8eeade815694d922be78a29101f89e5c3b6c8e
RaquelMarques97 <araquelfmarques@gmail.com>
2019-10-24 17:15:48 +0100

    styling and routing

---
+++ src/Challenge.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Challenge = () => (
+    <h1>Challenge</h1>
+);
+
+
+export default Challenge;
---
+++ src/WhereISS.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const WhereISS = () => (
+    <h1>WhereISS</h1>
+);
+
+export default WhereISS;
