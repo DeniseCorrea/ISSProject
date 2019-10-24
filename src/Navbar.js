b13523785c24cf61442a1017e54cfadf41ffec18
Jxnis <janis.ellerbrock@gmail.com>
2019-10-24 17:33:03 +0100

    adds NavLinks to Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 
 const Navbar = () => (
-
-    <ul>navbar</ul>
+    <nav>
+        <ul>
+            <li><NavLink exact to="/Homepage" activeClassName="selected">Homepage</NavLink></li>
+            <li><NavLink exact to="/WhereISS" activeClassName="selected">WhereISS</NavLink></li>
+            <li><NavLink exact to="/Challenge" activeClassName="selected">Challenge</NavLink></li>
+            <li><NavLink exact to="/Crew" activeClassName="selected">Crew</NavLink></li>
+            <li><NavLink exact to="/Contact" activeClassName="selected">Contact</NavLink></li>
+            <li><NavLink exact to="/Footer" activeClassName="selected">Footer</NavLink></li>
+        </ul>
+    </nav>
 );
 
 
