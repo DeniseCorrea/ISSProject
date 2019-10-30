4810530d6027219cefbb37eb4fe67d1dfa375a32
RaquelMarques97 <araquelfmarques@gmail.com>
2019-10-30 09:43:07 +0000

    installed map 2d

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, BrowserRouter, Route } from 'react-router-dom';
 import Navbar from './Navbar.js';
 import Homepage from './Homepage.js';
-import WhereISS from './WhereISS';
+import SimpleExample from './WhereISS.js';
 import Challenge from './Challenge.js';
 import Crew from './Crew.js';
 import Contact from './Contact.js';
@@ -12,19 +12,24 @@ import './App.css';
 
 function App() {
   return (
+
     <BrowserRouter>
       <Navbar />
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route exact path="/WhereISS" component={WhereISS} />
+        <Route exact path="/WhereISS" component={SimpleExample} />
         <Route exact path="/Challenge" component={Challenge} />
         <Route exact path="/Crew" component={Crew} />
         <Route exact path="/Contact" component={Contact} />
       </Switch>
       <Footer />
-
     </BrowserRouter>
+
+
+
   );
+
+
 }
 
 export default App;
