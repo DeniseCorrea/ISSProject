7705bc90a8763872457bebf6c322f6485ba2e350
RaquelMarques97 <araquelfmarques@gmail.com>
2019-10-30 18:14:17 +0000

    refined footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,16 +5,16 @@ import './Footer.css';
 const Footer = () => (
 
     <>
-        <footer>
+        <div className="footer">
             <ul className="contact">
                 <li><NavLink exact to="/Contact" activeClassName="selected">Contact</NavLink></li>
                 <li><NavLink exact to="/Contact" activeClassName="selected">About us</NavLink></li>
             </ul>
             <ul className="social">
-                <li><NavLink exact to="#" activeClassName="selected">instagram logo</NavLink></li>
-                <li><NavLink exact to="#" activeClassName="selected">facebook logo</NavLink></li>
+                <li><img src="https://image.flaticon.com/icons/png/512/87/87390.png" alt=""></img></li>
+                <li><img src="https://image.flaticon.com/icons/png/512/59/59439.png" alt=""></img></li>
             </ul>
-        </footer>
+        </div>
     </>
 );
 
