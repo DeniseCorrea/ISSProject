b6cf00c4b31ab30d95d5ff34f938f8f576bba0d9
Denise <denisewaskow@gmail.com>
2019-11-08 12:08:02 +0000

    Updates

---
+++ precache-manifest.fe3d7723a2bc51c6bef800c79b3f825a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f546e02dc2d219376825106c7f8cba1e",
+    "url": "/ISSProject/index.html"
+  },
+  {
+    "revision": "7fdc2886904ca26e72ae",
+    "url": "/ISSProject/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "dc307387c1ede5a834d5",
+    "url": "/ISSProject/static/css/main.5d5981db.chunk.css"
+  },
+  {
+    "revision": "7fdc2886904ca26e72ae",
+    "url": "/ISSProject/static/js/2.251a3c96.chunk.js"
+  },
+  {
+    "revision": "dc307387c1ede5a834d5",
+    "url": "/ISSProject/static/js/main.2ccec3a6.chunk.js"
+  },
+  {
+    "revision": "8c6428781454bb6d1c1e",
+    "url": "/ISSProject/static/js/runtime-main.6cdeede8.js"
+  },
+  {
+    "revision": "f0ac48082d44a769d257f22a48c0d324",
+    "url": "/ISSProject/static/media/iss.f0ac4808.png"
+  }
+]);
