6c2abf236bb35b357e9ea6e9431ad43e982304db
Denise <denisewaskow@gmail.com>
2019-11-08 10:44:29 +0000

    challenge structure

--- src/Image.js
+++ src/Image.js
@@ -2,57 +2,57 @@ import React from 'react';
 import './Image.css';
 
 class Image extends React.Component {
-  constructor(props){
-  super(props); 
-  this.state = {
-      copyright: '',
-      date:'',
-      title:'',
-      explanation: '',
-      url: '',
-      }
-  }
+    constructor(props){
+    super(props); 
+    this.state = {
+        copyright: '',
+        date:'',
+        title:'',
+        explanation: '',
+        url: '',
+        }
+    }
 
-  componentDidMount() {
-    this.getImages();
-  }
+    componentDidMount() {
+        this.getImages();
+    }
 
 
-  getImages = () => {
-      fetch ('https://api.nasa.gov/planetary/apod?api_key=DOJuuz8rSQdh6swCcE5Gk3NLsuMGNTBX8ubSheVQ')
-      .then (response => response.json())
-      .then (data => {
-          this.setState({
-              copyright:data.copyright,
-              date: data.date,
-              title: data.title,
-              explanation: data.explanation,
-              url: data.url,
-          })
-      })
-  }
+    getImages = () => {
+        fetch ('https://api.nasa.gov/planetary/apod?api_key=DOJuuz8rSQdh6swCcE5Gk3NLsuMGNTBX8ubSheVQ')
+        .then (response => response.json())
+        .then (data => {
+            this.setState({
+                copyright:data.copyright,
+                date: data.date,
+                title: data.title,
+                explanation: data.explanation,
+                url: data.url,
+            })
+        })
+    }
 
   
-  render () {
-      return (
-        <div className="homepage">
-            <div className="homepage-title">
-                <h1>Fly By</h1>
+    render () {
+        return (
+            <div className="homepage">
+                <div className="homepage-title">
+                    <h1>Fly By</h1>
+                </div>
+                <div className="image-title">
+                    <p>{this.state.title}</p>
+                </div>
+                <div className="image-picture">
+                    <img src={this.state.url} alt='' />
+                </div>
+                <div className="image-infos">
+                    <p>Copyright: {this.state.copyright}</p> 
+                    <p>{this.state.date}</p>
+                    <p>{this.state.explanation}</p>
+                </div>
             </div>
-            <div className="image-title">
-                <p>{this.state.title}</p>
-            </div>
-            <div className="image-picture">
-                <img src={this.state.url} alt='' />
-            </div>
-            <div className="image-infos">
-                <p>Copyright: {this.state.copyright}</p> 
-                <p>{this.state.date}</p>
-                <p>{this.state.explanation}</p>
-            </div>
-        </div>
-    )
-  }
+        )
+    }
 };
 
 
---
+++ src/questbank.js
@@ -0,0 +1,38 @@
+const questionsFromQuestBank =   
+[ 
+    {
+        "question": "When was the ISS launched?",
+        "answers": ["1984", "1986", "2019", "2018"],
+        "correct": "1984",
+        "questionID": "75846" 
+    },
+    {
+        "question": "What is the color of Earth?",
+        "answers": ["green", "blue", "red", "yellow"],
+        "correct": "blue",
+        "questionID": "90856" 
+    },
+    {
+        "question": "When was the first step on the moon?",
+        "answers": ["1932", "1995", "1969", "2000"],
+        "correct": "1969",
+        "questionID": "45636" 
+    },
+    {
+        "question": "How many countries participate in ISS?",
+        "answers": ["15", "9", "22", "10"],
+        "correct": "15",
+        "questionID": "64756" 
+    },
+    {
+        "question": "How many times a day does ISS orbit the Earth?",
+        "answers": ["8", "34", "16", "25"],
+        "correct": "16",
+        "questionID": "90382" 
+    }
+]
+
+export default questionsFromQuestBank;
+
+
+
