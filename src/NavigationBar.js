bca21ab33009450be8537f96ef7d5410e28f911f
Jxnis <janis.ellerbrock@gmail.com>
2019-11-13 16:59:09 +0000

    adds new navbar and logo style

--- src/NavigationBar.js
+++ src/NavigationBar.js
@@ -7,7 +7,7 @@ import './NavigationBar.css';
 
 
 const Menu = styled.div`
-  border-bottom: 4px solid MediumPurple;
+  border-bottom: 4px solid #b2ebf9;
   background-color: black;
   ul {
     padding-top: 20px;
@@ -23,13 +23,15 @@ const Menu = styled.div`
     text-decoration: none;
     text-transform: uppercase;
     font-size: 23px;
-    color: MediumPurple;
+    color: #b2ebf9;
     }
 
     a:hover {
         color: white;
         background-color: black;  
     }
+
+    
   
   @media (max-width: 750px) {
     padding: 10px 0;
@@ -51,20 +53,19 @@ class NavigationBar extends Component {
         return (
         <div>
             <ResponsiveMenu
-              menuOpenButton={<FaBars size={45} color="MediumPurple" />}
-              menuCloseButton={<FaTimes size={45} color="MediumPurple" />}
+              menuOpenButton={<FaBars size={45} color="#b2ebf9" />}
+              menuCloseButton={<FaTimes size={45} color="#b2ebf9" />}
               changeMenuOn="750px"
               largeMenuClassName="large-menu"
               smallMenuClassName="small-menu"
               menu={
                 <Menu>
                   <ul>
-                    <li><NavLink exact to="/" activeClassName="active"><img src="https://res.cloudinary.com/dzvkdhu9c/image/upload/v1573582930/ISS/ee100ff4-c375-4258-9e2d-de5ee7240801_200x200_pguolh.png" className="logo"/></NavLink></li>
-                    <li><NavLink exact to="/WhereISS" activeClassName="selected">WhereISS</NavLink></li>
+                    <li><NavLink exact to="/WhereISS" activeClassName="selected">ISS</NavLink></li>
                     <li><NavLink exact to="/Challenge" activeClassName="selected">Challenge</NavLink></li>
+                    <li><NavLink exact to="/" activeClassName="active"><img src="https://res.cloudinary.com/dzvkdhu9c/image/upload/v1573582930/ISS/ee100ff4-c375-4258-9e2d-de5ee7240801_200x200_pguolh.png" className="logo"/></NavLink></li>
                     <li><NavLink exact to="/Sighting" activeClassName="selected">Sighting</NavLink></li>
                     <li><NavLink exact to="/Crew" activeClassName="selected">Crew</NavLink></li>
-                    <li><NavLink exact to="/Contact" activeClassName="selected">Contact</NavLink></li>
                   </ul>
                 </Menu>
               }
