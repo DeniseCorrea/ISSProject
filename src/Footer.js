9fd99b6dbe30d594f441d4171baef781e9aed95f
RaquelMarques97 <araquelfmarques@gmail.com>
2019-11-20 00:18:50 +0000

    add marker to globe

--- src/Footer.js
+++ src/Footer.js
@@ -13,8 +13,8 @@ const Footer = () => (
 
             <div className="social">
                 <ul >
-                    <li><a href="https://www.instagram.com/nasa/?hl=en"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkkh9W8gx7D4smk8lf52SVKoHiXnCsaH14nbT5_-6SOupeEyB1Bg&s" alt=""></img></a></li>
-                    <li><a href="https://www.facebook.com/nasasolarsystem/"><img src='https://www.iconsdb.com/icons/preview/white/facebook-4-xxl.png' alt=""></img></a></li>
+                    <li><a href="https://www.instagram.com/nasa/?hl=en"><img src="instagram.png" alt=""></img></a></li>
+                    <li><a href="https://www.facebook.com/nasasolarsystem/"><img src='facebook.png' alt=""></img></a></li>
                 </ul>
             </div>
         </div>
